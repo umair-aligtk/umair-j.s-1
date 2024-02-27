d0fbada1aa6ed9ccab4452c5c9d19a63ec818c85
umair-aligtk <156018837+umair-aligtk@users.noreply.github.com>
2024-02-27 10:10:15 +0500

    web

---
+++ app.js
@@ -0,0 +1,62 @@
+//Chapter:1
+
+//Q:no:2
+//var name="Error! Please entera valid password"
+//alert(name)
+
+//Q:no:3
+//var welcome="Wel come to js land....\n Happy coding!"
+//alert(welcome)
+
+//Q:no:4
+//var land="Wel come to js land...."
+//alert(land)
+//var happy="happy coding! \n Prevent this page from creaying additional dialogs"
+//alert(happy)
+//var hello="Hello I can run through my web browser's cansole"
+//console.log(hello)
+
+//Chapter:2
+
+//Q:no:1
+//var user  ="username"
+//alert(user)
+//Q:no:2
+//var myname  ="Umair Ali"
+//alert(myname)
+//Q:no:3
+//var message ="17 years old"
+//alert(message)
+//Q:no:3
+//var certificate =" Certified mobile application Development"
+//alert(certificate)
+
+//var email ="My email adress is umairali0g0@gmail.com"
+//alert(email)
+
+//var pizza =" PIZZA \n PIZZ \n PIZ  \n PI   \n P  "
+//alert(pizza)
+
+
+//var book =" I am trying to learn from the Book a smaller \n way to learn javascript"
+//alert(book)
+
+//var image = "“▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬”"
+//alert(image)
+
+//chapter:3
+
+//var text =" Yah! I can write HTML content through java script"
+//document.write(text)
+
+//var birthyear ="My birth year is 2007 <br> Data type of my declare variable is number"
+//document.write(birthyear)
+
+//var cloth =" <b>Umair ali</b> ordered  <b>5 T-shirt (s)</b> on xyz clothing store"
+//document.write(cloth)
+
+var num1 = 2
+var num2 = 3
+var result = num1 * num2
+alert(result)
+
